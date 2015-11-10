85f7dbace7d3e1715811a654151b0d6acedde321
Chris <chris@strike.ly>
2015-11-10 21:32:43 +0000

    Add frontage

--- scraper.py
+++ scraper.py
@@ -29,11 +29,16 @@ def teardown_request(exception):
 
 from contextlib import closing
 
+
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/urls')
 def show_url():
